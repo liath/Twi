e71d64c0f964d5a5ceab729c2a1b41460b5bc809
Liath <liath@exthilion.org>
2012-05-26 18:52:09 -0300

    Initial

---
+++ public/js/site.js
@@ -0,0 +1,17 @@
+$(function(){
+    if((typeof($.browser.msie) != "undefined" && $.browser.msie) && (typeof(Modernizr) == "undefined" || !Modernizr.input.placeholder)){
+        $("input").each(
+            function(){
+                if($(this).val()=="" && $(this).attr("placeholder")!=""){
+                    $(this).val($(this).attr("placeholder"));
+                    $(this).focus(function(){
+                        if($(this).val()==$(this).attr("placeholder")) $(this).val("");
+                    });
+                    $(this).blur(function(){
+                        if($(this).val()=="") $(this).val($(this).attr("placeholder"));
+                    });
+                }
+            });
+
+    }
+});
